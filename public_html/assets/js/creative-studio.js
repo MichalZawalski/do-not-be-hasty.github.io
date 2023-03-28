859c0d26b73d990cad3f212e2a6b061ba5f70854
Michał Zawalski <michal.zawalski25@gmail.com>
2023-03-28 19:02:06 +0200

    Preview page.

---
+++ public_html/assets/js/creative-studio.js
@@ -0,0 +1,43 @@
+/*!
+=========================================================
+* Creative Studio Landing page
+=========================================================
+
+* Copyright: 2019 DevCRUD (https://devcrud.com)
+* Licensed: (https://devcrud.com/licenses)
+* Coded by www.devcrud.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/ 
+
+// smooth scroll
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+});
+
+$('#playIntroButton').click(function () {
+    $('#videoBar').slideDown();
+    $('#playBar').slideUp();
+    $('#introVideo').get(0).play()
+});
+
+$('#introVideo').on("ended", function () {
+    $('#videoBar').slideUp();
+    $('#playBar').slideDown();
+})
